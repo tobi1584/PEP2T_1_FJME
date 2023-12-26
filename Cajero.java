93ceea63ca953ea45531f0b2539990f21201b162
tobi1584 <146116182+tobi1584@users.noreply.github.com>
2023-12-26 14:04:29 +0100

    Código terminado, falta comentarlo

--- Cajero.java
+++ Cajero.java
@@ -1,56 +1,94 @@
-import java.util.Scanner;  // Importación de la clase Scanner para entrada de datos
+import java.util.Scanner;
 
-// Clase del programa
 public class Cajero {
 
-    // Método principal
-    public static void main(String[] args) {
-        // Atributos - Aquí se obtienen y asignan valores a partir de los argumentos pasados al programa
-        float verif_saldo = Float.parseFloat(args[1]);  // Se convierte el segundo argumento a un número decimal
-        int verif_tope = Integer.parseInt(args[0]);  // Se convierte el primer argumento a un número entero
+    private float saldo;
+    private float tope;
+    private Scanner teclado = new Scanner(System.in);
 
-        // Llamada al método del menú
-        menu();
+    private Cajero(float saldo, float tope) {
+        this.saldo = saldo;
+        this.tope = tope;
     }
 
-    // Método del menú
-    public static void menu() {
-        System.out.println("\t\t\t\t\033[4mPROGRAMA CAJERO AUTOMÁTICO\033[0m");  // Encabezado del programa
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Meta usted los valores de su cuenta y el tope que le quiera agregar");
+            System.exit(-1);
+        }
 
-        boolean bucle = true;  // Variable para controlar el bucle del menú
+        float saldo = Float.parseFloat(args[0]);
+        float tope = Float.parseFloat(args[1]);
+        Cajero cuentaBancaria = new Cajero(saldo, tope);
+        cuentaBancaria.menu();
+    }
+
+    private void menu() {
+        System.out.println("\t\t\t\t\033[4mPROGRAMA CAJERO AUTOMÁTICO\033[0m");
+
+        boolean bucle = true;
         while (bucle) {
             // Menú de opciones
             System.out.println("\n\t\t\t\tMenú de Opciones");
             System.out.println("\t\t\t\t================\n");
-            System.out.println("1) Retirada de dinero");
-            System.out.println("2) Ingreso de dinero");
-            System.out.println("3) Salir");
+            System.out.println("\t\t\t\t1) Retirada de dinero");
+            System.out.println("\t\t\t\t2) Ingreso de dinero");
+            System.out.println("\t\t\t\t3) Salir");
+            System.out.print("\nDiga la opción que desea usar: ");
+            String opcion = teclado.nextLine();
+            System.out.println();
 
-            // Selector de opción por parte del usuario
-            System.out.println("\nDiga la opción que desea usar: ");
-            int opcion;
-            Scanner teclado = new Scanner(System.in);
-            opcion = teclado.nextInt();  // Se lee la opción ingresada por el usuario
-
-            // Bucle para validar la opción ingresada por el usuario
-            while (opcion < 1 || opcion > 3) {
-                System.out.println("¡Vaya esa opción no es válida, intentelo de nuevo!");
-                System.out.println("\nDiga la opción que desea usar: ");
-                opcion = teclado.nextInt();  // Se vuelve a leer la opción si es inválida
+            switch (opcion) {
+                case "1" -> retirada();
+                case "2" -> ingreso();
+                case "3" -> bucle = false;
+                default -> System.out.println("¡Vaya esa opción no es válida, intentelo de nuevo!");
             }
-            // Se muestra la opción escogida por el usuario
-            System.out.println("\nOpción: " + opcion);
-
-            // Lógica para cada opción seleccionada por el usuario
-            if (opcion == 1)
-                retirada();  // llamada al método retirada
-            else if (opcion == 2)
-                System.out.println("Adios");  // llamad al método ingreso
-            else
-                bucle = false;  // Termina el bucle
         }
+
     }
-    public static void retirada(){
-        System.out.println();
+
+    private void retirada() {
+        verif_saldo();
+        funcionRetirada();
+        verif_saldo();
+    }
+
+    private void ingreso(){
+        verif_saldo();
+        funcionIngreso();
+        verif_saldo();
+    }
+
+    private void verif_saldo() {
+        System.out.println("\nSu saldo actual es de: " + saldo + "€");
+    }
+
+    private void verif_tope(){
+        System.out.println("\nTiene establecido ahora un tope de " + tope + "€");
+    }
+
+    private void funcionIngreso(){
+        System.out.println("Teclee dinero a ingresar:");
+        int opcion = teclado.nextInt();
+        teclado.nextLine();
+        System.out.println("\nUsted ingresó " + opcion +"€");
+        saldo += opcion;
+    }
+
+    private void funcionRetirada(){
+        System.out.println("Teclee dinero a retirar:");
+        int opcion = teclado.nextInt();
+        teclado.nextLine();
+        if (opcion > tope) {
+            System.out.println("\nIntenta retirar " + opcion + "€");
+            verif_tope();
+            verif_saldo();
+
+        } else if (opcion <= tope){
+            System.out.println("\nUsted retiró " + opcion +"€");
+            saldo -= opcion;
     }
 }
+}
+
